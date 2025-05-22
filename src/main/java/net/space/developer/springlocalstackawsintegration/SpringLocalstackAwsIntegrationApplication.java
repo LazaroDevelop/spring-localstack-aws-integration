3212f5a01da2144e955f502b9623ad14acc300c0
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-22 10:32:12 -0400

    initial commit

---
+++ src/main/java/net/space/developer/springlocalstackawsintegration/SpringLocalstackAwsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package net.space.developer.springlocalstackawsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLocalstackAwsIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLocalstackAwsIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/space/developer/springlocalstackawsintegration/SpringLocalstackAwsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.space.developer.springlocalstackawsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLocalstackAwsIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
