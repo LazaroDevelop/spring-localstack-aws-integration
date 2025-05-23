d73befa2f7f0a51fb83ca451df95ee5fe36a5040
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-23 10:27:30 -0400

    add enable configuration properties

--- src/main/java/net/space/developer/springlocalstackawsintegration/SpringLocalstackAwsIntegrationApplication.java
+++ src/main/java/net/space/developer/springlocalstackawsintegration/SpringLocalstackAwsIntegrationApplication.java
@@ -2,8 +2,11 @@ package net.space.developer.springlocalstackawsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SpringLocalstackAwsIntegrationApplication {
 
     public static void main(String[] args) {
